f4e55d902ed2a77d9a8b594c755403eff93428a4
TheOtherDavid <davidhand1988@gmail.com>
2018-01-29 01:18:25 -0500

    Renaming DB_build, creating main method,

--- db_build.py
+++ db_build.py
@@ -3,6 +3,7 @@ import time
 import dbservice
 import musicservice
 
+
 def call_api_for_list(artists_to_call):
     time_of_last_api_call = time.time()
     counter = 0
@@ -29,20 +30,26 @@ def call_api_for_list(artists_to_call):
         counter += 1
 
 
-#Re-seeding from scratch for testing purposes
-#dbservice.seed_db_for_test()
+def main():
+    #Re-seeding from scratch for testing purposes
+    #dbservice.seed_db_for_test()
 
 
-if len(sys.argv) >= 2:
-    NUMBER_OF_ARTISTS = int(sys.argv[1])
-else:
-    #Default 100, put in arguments for more
-    NUMBER_OF_ARTISTS = 100
-#Call the database for the number of artists that need to be called
-ARTISTS_TO_CALL = dbservice.get_artists_to_call(NUMBER_OF_ARTISTS)
+    if len(sys.argv) >= 2:
+        NUMBER_OF_ARTISTS = int(sys.argv[1])
+    else:
+        #Default 100, put in arguments for more
+        NUMBER_OF_ARTISTS = 10
+    #Call the database for the number of artists that need to be called
+    ARTISTS_TO_CALL = dbservice.get_artists_to_call(NUMBER_OF_ARTISTS)
 
-print('Making ' + str(NUMBER_OF_ARTISTS) + ' API calls')
+    print('Making ' + str(NUMBER_OF_ARTISTS) + ' API calls')
 
-call_api_for_list(ARTISTS_TO_CALL)
+    call_api_for_list(ARTISTS_TO_CALL)
 
-print('Maximum number of iterations reached. Aborting.')
+    print('Maximum number of iterations reached. Aborting.')
+
+
+
+if __name__ == "__main__":
+    main()
