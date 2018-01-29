456ccc0a8f40b41e744c9fb9e32fe7bf1f289640
TheOtherDavid <davidhand1988@gmail.com>
2018-01-28 23:11:09 -0500

    Adding functionality to output playlist as a file, instead of just in the logs.

--- tuneup.py
+++ tuneup.py
@@ -14,11 +14,12 @@ if len(sys.argv) >= 2:
 #TODO: Pass in the name of the CSV file as an argument
 
 #Testing read CSV
-filename = 'trimmed-playlist.csv'
+FILENAME = 'trimmed-playlist'
+FILETYPE = '.csv'
 song_list = []
-with open(filename, 'r') as file:
-    reader = csv.reader(file, delimiter='|')
-    for row in reader:
+with open(FILENAME + FILETYPE, 'r') as file:
+    READER = csv.reader(file, delimiter='|')
+    for row in READER:
     #Make a Song out of each row, add to list
         song_row = song.make_song(row[0], row[1])
         song_list.append(song_row)
@@ -27,10 +28,15 @@ for song in song_list:
     #Print the Songs from the Song object
     print(song.name + ' by ' + song.artist.name)
 
-solution = algorithm.nearest_neighbors(song_list)
+SOLUTION = algorithm.nearest_neighbors(song_list)
 
 print('Solution found:')
-print(', '.join(str(x) for x in solution))
+print(', '.join(str(x) for x in SOLUTION))
 print('Full solution took ' + str(time.time()-start_time))
 
 #TODO: write a new CSV file with the properly-ordered solution
+
+with open(FILENAME + '-solved' + FILETYPE, "w", newline='') as file:
+    WRITER = csv.writer(file, delimiter='|')
+    for song in SOLUTION:
+        WRITER.writerow([str(song), str(song.artist.name)])
