3d89c81f695fb909f424dd68b119b0e7ff7fe508
TheOtherDavid <davidhand1988@gmail.com>
2018-02-03 19:04:23 -0500

    Stubbed out spotify_service.py

---
+++ spotify_service.py
@@ -0,0 +1,18 @@
+
+#Make the bearer token call up here, so it'll be done when the module is initialized
+
+def read_songs_from_playlist(user_id,playlist_id):
+    #Make playlist call with user_id & playlist_id
+    #Read out songs from response
+
+    #for track in tracks.items.track:
+        #song.name = track.name
+        #song.artist.name = track.artists[0].name
+        #song.spotify_id = track.id
+        #song_list.append(song)
+        
+        #We'll need to keep track of the spotifyID of these songs too, to create the playlist.
+    return None
+
+def write_playlist(song_list):
+    return None
